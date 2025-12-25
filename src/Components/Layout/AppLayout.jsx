a33a3ce1b3305918e7a7d0eab42c2f8ba6a8e8e1
Bushi2001 <shivjha1983@gmail.com>
2025-12-25 21:03:07 +0530

    moreover

--- src/Components/Layout/AppLayout.jsx
+++ src/Components/Layout/AppLayout.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Snowfall from "react-snowfall";
 import Headers from "../UI/Headers";
 import Footers from "../UI/Footers";
 
 const AppLayout = () => {
   return (
     <div className="min-h-screen">
+      {/* Festive snowfall effect ❄️ */}
+      <Snowfall
+        color="white"
+        snowflakeCount={150}
+        style={{
+          position: "fixed",
+          width: "100vw",
+          height: "100vh",
+          zIndex: 9999,
+          pointerEvents: "none", // Allow clicking through the snowflakes
+        }}
+      />
       <Headers />
       <Outlet />
       <Footers />
